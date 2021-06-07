373981bedcf07035d664a2903014373b80a2c3bd
Malik H <malik.h@webdevpro.net>
2021-06-07 09:47:37 +0200

    start

---
+++ jour1/00-start.php
@@ -0,0 +1,15 @@
+<?php 
+/* 
+1 démarrer votre serveur local => XAMPP / MAMP
+
+2 démarrer les services => Apache et MySQL 
+
+3 dans le serveur créer le dossier projet-html => (bien respecter les minuscules et pas d'espace)
+4 créer un sous dossier => jour1  
+
+5 l'objectif de la journée c'est de faire la liste des fonctions de php que l'on va utiliser pour créer un projet => blog avec un back office créer des articles 
+rappel en php 
+
+=> créer le fichier 01-variable-constantes.php
+
+*/
---
+++ jour1/01-variable-constantes.php
@@ -0,0 +1,53 @@
+<?php 
+
+// conseillé => dans le fichier que l'on écrire => unique écrire du PHP 
+// pas html il est conseillé de ne pas fermer la balise PHP 
+
+// import via de fichiers via  require() => 
+// et si dans l'un de ces fichiers importés on utilise la $_SESSION et qu'il y a un espace avant ce fichier 
+// erreur de type header()
+
+// règle importante => ouvrez un fichier .php => 
+// commencer dès le 1er caractère 
+// pas d'espace avant 
+// pas de balise fermante => pour éviter ce type d'erreur 
+
+// créer une variable en php 
+
+$premier = 2 ; // créer une variable 
+
+// il est possible de stocker dans les variables en PHP plusieurs types de variables 
+
+// chaine de caractères => string 
+$phrase = "bonjour" ; // Attention aux ; => ils sont OBLIGATOIRES 
+$phrase2 = 'hello' ;
+
+// addition de chaine de caractères = concaténation 
+// 1ère différence entre les string à base de "" et les '' 
+
+$introduction = "$phrase les amis"; // avec les guillemets => mettez directement les variables dans les guillements 
+
+$saluation = $phrase . ' comment allez vous ??'; // avec les apostrophes => utiliser le symbole . sortir les variables pour réaliser une concaténation 
+
+// il est possible de sauter des lignes dans les "" 
+
+$commentaire = "$phrase
+                je suis un commentaire "; // pas problème pour sauter une ligne 
+
+// pas possible de le faire avec des chaines de caractères à base d'apostrophe 
+
+$commentaire2 = $phrase . ' 
+               une erreur ';
+
+// MAMP
+// http://localhost:8888/projet-html/jour1/01-variable-constantes.php
+// XAMPP
+// http://localhost/projet-html/jour1/01-variable-constantes.php
+
+// chiffres 
+$nombre = 2; // entier
+$prix = 12.5; // float
+$temperature = -5; // entier négatif
+
+
+
