0b11477858b6b4f5cd5d29aa9b47c886053b1f42
Malik H <malik.h@webdevpro.net>
2021-06-07 15:09:42 +0200

    exo 10

---
+++ jour1/10-exo.php
@@ -0,0 +1,28 @@
+<?php
+// http://localhost/projet-html/jour1/10-exo.php
+$chiffre1 = "2.2" ;
+$chiffre2 = 2 ;
+$chiffre3 = $chiffre2 + $chiffre1 ; // "2.2" + 2 => 2 + 2 => integer / double
+$chiffre4 = $chiffre2 === $chiffre1 ; 
+$chiffre5 = $chiffre1 - $chiffre2; // 2  -  "2.2" => 2 - 2.2 =  -0.2
+                                    // transtypage automatique du langage PHP
+
+printf( 'le type de $chiffre1 est %s <br>' ,  gettype($chiffre1) ); // echo 
+printf( 'le type de $chiffre2 est %s <br>' ,  gettype($chiffre2) ); // echo 
+printf( 'le type de $chiffre3 est %s <br>' ,  gettype($chiffre3) ); // echo 
+printf( 'le type de $chiffre4 est %s <br>' ,  gettype($chiffre4) ); // echo 
+printf( 'le type de $chiffre5 est %s <br>' ,  gettype($chiffre5) ); // echo  double
+
+
+$premier = "Bonjour";
+$deuxieme = 3;
+
+if(gettype($premier) === "string" && strlen($premier) === 20 ){
+    echo '$premier est conforme <br>' ;
+}
+
+if(gettype($deuxieme) === "string" || strlen($deuxieme) >= 3 ){
+    echo '$deuxieme est quasi conforme <br>' ;
+}else {
+    echo '$deuxieme n\'est pas conforme à 100%' ;
+}
