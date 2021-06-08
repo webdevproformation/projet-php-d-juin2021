d49abf8605e9daefff741dcb658840ddf9c7af5a
Malik H <malik.h@webdevpro.net>
2021-06-08 14:33:34 +0200

    modification dispatcher

--- tp-1/index.php
+++ tp-1/index.php
@@ -3,18 +3,22 @@
 
 if(isset($_GET["p"])){
 
-   $params = explode("/",$_GET["p"]);
+   $params = explode("/",trim($_GET["p"] , "/"));
 
    /* var_dump($params); */
-   $controller = $params[0]; //accueil
-   $method = $params[1];  // index
+   $controller = strtolower( $params[0] ); //accueil
+   $method = strtolower( isset($params[1]) ? $params[1] : "index"  );  // index
 
-   // charger le fichier qui controller qui est stocké dans le dossier controller
-   require "controller/".$controller . ".class.php";
-
-   $controllerName = ucfirst($controller)."Controller";
-
-   $c= new $controllerName(); // $c = new AccueilController();
-   $c->$method(); // $c->index();
-
+   $controller_file = "controller/".$controller . ".class.php";
+   if(file_exists($controller_file)){
+        // charger le fichier qui controller qui est stocké dans le dossier controller
+        require $controller_file;
+        $controllerName = ucfirst($controller)."Controller";
+        $c= new $controllerName(); // $c = new AccueilController();
+        $c->$method(); // $c->index();
+   }else {
+    echo "le fichier n'existe pas";
+    die();
+   }
+   
 }
