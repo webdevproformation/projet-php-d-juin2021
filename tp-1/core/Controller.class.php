a287bf653dcf5a89fb7f325627d9d6f304682a10
Malik H <malik.h@webdevpro.net>
2021-06-08 16:29:26 +0200

    render dynmatique en fonction du controller qui l'appel

--- tp-1/core/Controller.class.php
+++ tp-1/core/Controller.class.php
@@ -4,7 +4,13 @@ class Controller{
 
     public function render($nomView){
 
-        require "view/accueil/$nomView.php";
+        // méthode render qui est partagé sur les controller 
+        // 
+        // var_dump(get_class($this));
+        $dossier =  str_replace("Controller", "" , get_class($this)) ;
+        $dossier = strtolower($dossier);
+
+        require "view/$dossier/$nomView.php";
 
     }
 
