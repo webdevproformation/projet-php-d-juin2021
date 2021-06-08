2c02966748d96c80545cabc06aed8aec4972f42a
Malik H <malik.h@webdevpro.net>
2021-06-08 15:11:02 +0200

    fin du dispatcher

--- tp-1/index.php
+++ tp-1/index.php
@@ -5,8 +5,9 @@ if(isset($_GET["p"])){
 
    $params = explode("/",trim($_GET["p"] , "/"));
 
-   /* var_dump($params); */
-   $controller = strtolower( $params[0] ); //accueil
+   //var_dump($params); 
+
+   $controller = strtolower( isset($params[0]) && !empty($params[0]) ? $params[0] : "accueil" ); //accueil
    $method = strtolower( isset($params[1]) ? $params[1] : "index"  );  // index
 
    $controller_file = "controller/".$controller . ".class.php";
@@ -15,16 +16,20 @@ if(isset($_GET["p"])){
         require $controller_file;
         $controllerName = ucfirst($controller)."Controller";
         $c= new $controllerName(); // $c = new AccueilController();
-        
-        if(method_exists($c , $method  )){
-            $c->$method(); // $c->index();
+
+        if(method_exists($c , $method )){
+            // http://localhost/projet-html/tp-1/accueil/index
+            // Framework PHP => Symfony Laravel CakePHP CodeIgnity
+            $c->$method(); // $c->index(); 
         }else{
+            //http://localhost/projet-html/tp-1/accueil/toto
             header("HTTP/1.0 404 Not Found");
             require "view/404.php";
             die();
         }
    } else {
        // si le controller appelé n'existe pas alors => page d'erreur 404 
+        // http://localhost/projet-html/tp-1/blabla
         header("HTTP/1.0 404 Not Found");
         require "view/404.php";
         die();
