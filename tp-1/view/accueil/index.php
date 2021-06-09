510019c5043d2bceaa9c8c011f6a7ce35f2fe911
Malik H <malik.h@webdevpro.net>
2021-06-09 11:41:17 +0200

    afficher les articles dans la vue de l'accueil

--- tp-1/view/accueil/index.php
+++ tp-1/view/accueil/index.php
@@ -1,1 +1,15 @@
 <h1>Bienvenu sur mon site jour 2</h1>
+<?php // var_dump($articles) ?>
+<?php foreach($articles as $article  ) : ?>
+    <div class="col-4">
+        <article class="card mb-3">
+            <header class="card-header">
+                <h2><?= $article["titre"] ?></h2>
+            </header>
+            <img src="http://via.placeholder.com/300x100?text=<?= $article["id"] ?>" alt="">
+            <div class="card-body">
+                <?= $article["contenu"] ?>
+            </div>
+        </article>
+    </div>
+<?php endforeach ?>
