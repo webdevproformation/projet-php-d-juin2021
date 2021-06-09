81658626ecf85924b621fdd5adf14fb9cdb5ae27
Malik H <malik.h@webdevpro.net>
2021-06-09 11:24:26 +0200

    présentation de extract

--- jour3/04-extract.php
+++ jour3/04-extract.php
@@ -23,3 +23,29 @@ $adresse  = "75 rue des Long Près à Paris"; */
 
 var_dump($aliments[0]);
 var_dump($budget);
+
+// par rapport à la variable $data dans le fichier accueil.class.php dans la méthode index ?? @ quoi avec ressemble $data après un extract ??
+
+$data = [ 
+    "articles" => [
+        [ "titre" => "article 1" , "contenu" => "lorem ipsum" , "id" => 1],
+        [ "titre" => "article 2" , "contenu" => "lorem ipsum" , "id" => 2],
+        [ "titre" => "article 3" , "contenu" => "lorem ipsum" , "id" => 3],
+    ]
+];
+
+extract($data);
+var_dump(extract($data)); // 1
+
+$articles = [
+    [ "titre" => "article 1" , "contenu" => "lorem ipsum" , "id" => 1],
+    [ "titre" => "article 2" , "contenu" => "lorem ipsum" , "id" => 2],
+    [ "titre" => "article 3" , "contenu" => "lorem ipsum" , "id" => 3],
+];
+
+var_dump(extract($data["articles"][0])); // 3
+var_dump(extract($data["articles"])); // => 0
+
+/* $titre = "article 1";
+$contenu = "lorem ipsum";
+$id = 1; */
